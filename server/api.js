1a6abb63db9123856733a21d9173bc8291942091
omerffaruk <farukyamak11044@gmail.com>
2022-03-01 17:04:28 +0000

    feat: handle get and post requests to the /api endpoint

--- server/api.js
+++ server/api.js
@@ -2,8 +2,42 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (_, res) => {
-	res.json({ message: "Hello, world!" });
+router.get("/", (req, res) => {
+  const query = 'SELECT * FROM videos';
+  pool.query(query, (error, result) => {
+    if(error) {
+      console.log(error);
+      return res.send(error);
+    } else {
+      res.json(result.rows);
+    }
+  })
 });
+
+// POST "/"
+router.post('/', (req, res) => {
+  const { title, url} = req.body;
+  const newVideo = {
+    "title": title,
+    "url": url,
+    "rating": 0
+  }
+  if(!title || !url) {
+    return res.status(400).send({
+  "result": "failure",
+  "message": "Video could not be saved"
+})
+  } else {
+    pool.query('SELECT * FROM videos WHERE title=$1 AND url=$2', [title, url])
+    .then((result) => {
+      if(result.rows.length > 0) {
+        return res.status(400).send(`This video with the title of ${title} and url of ${url} is already in videos.`)
+      } else {
+        pool.query('INSERT INTO videos (title, url, rating) VALUES ($1, $2, 0)', [title, url])
+        .then(() => res.status(202).send(`The video has been added to the videos.`));
+      }
+    })
+  }
+}) 
 
 export default router;
